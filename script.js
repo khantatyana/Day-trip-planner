309bb867b93d8a215591acb8953777673ee4e9e7
Tatyana <hanyatan@gmail.com>
2020-06-17 21:15:49 -0400

    added testing map element to the detail.html

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-var queryURL = "https://developer.nps.gov/api/v1/parks?api_key=9bu5bi3vaKYgYQt7Cj4pxdYFN8pkwsL9zSIiRFEd"
+var apiKey = "9bu5bi3vaKYgYQt7Cj4pxdYFN8pkwsL9zSIiRFEd";
+
+// here should be $("#stateButton").val(); or stateCode from AJAX call
+// var stateCode = $("#stateButton").val();
+
+
+var queryURL = "https://developer.nps.gov/api/v1/parks?api_key=" + apiKey + "&stateCode=" + stateCode;
+
